910a3d76f14c3f447d99737d6c772300aba25f74
Ahmad Ibraheem <ahmad@wortel.ai>
2022-10-17 12:32:02 +0500

    latest commit

--- app.py
+++ app.py
@@ -7,14 +7,24 @@ app = Flask(__name__)
 host = "localhost"
 port = 6333
 COLLECTION_NAME = "images"
+DATA_DIR = "/home/ahmad/Downloads/Qdrant_images/coin_data/coin_images/"
 
 embeddings = Embeddings()
 collection = Qdrant(host, port, embeddings)
 
-@app.route('/')
-def hello_world():   
+@app.route('/count')
+def count():   
     vector_count = collection.get_collection_count(COLLECTION_NAME)
     return f"count of vectors in {COLLECTION_NAME}: {vector_count}"
+
+@app.route("/search/<search_img_path>")
+def search(search_img_path):   
+    search_img_path = DATA_DIR+f"{search_img_path}"
+    results = collection.search(
+        search_img_path, COLLECTION_NAME, limit=10, offset=0, threshold=0
+    )
+    return f"{results}"
+    return results
 
 
 if __name__=="__main__":
